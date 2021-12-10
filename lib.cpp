e882e62500c61069f665429033ac12f65a995260
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-11 00:24:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+#include <cstring>
 
+bool ricerca_nome (char lista_nomi[10][20],char nome[1][20], int &pos){
+    for (int i = 0; i < 10; i++){
+        if(stricmp(lista_nomi[i], nome[0])==0){
+        pos=i;
+        return true;
+        }
+    }
+    return false;
+}
