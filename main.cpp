8bec09cda92a5678620feed2bea709d43da2a8ce
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-11 00:25:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int pos=0;
+    char lista_nomi [10][20], nome [1][20];
+    for (int i = 0; i < 10; i++) {
+        cin >> lista_nomi[i];
+    }
+        cin >> nome[0];
+
+       if (ricerca_nome(lista_nomi, nome, pos)){
+        cout << pos << endl;
+    } else{
+    cout << "non presente" <<endl;
+       }
+       return 0;
 }
