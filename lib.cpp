9dd86c8e634051e6e249e5fabc462cc7f0cc7b8a
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-11 00:27:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,7 @@
 
 bool ricerca_nome (char lista_nomi[10][20],char nome[1][20], int &pos){
     for (int i = 0; i < 10; i++){
-        if(stricmp(lista_nomi[i], nome[0])==0){
+        if(strcmp(lista_nomi[i], nome[0])==0){
         pos=i;
         return true;
         }
